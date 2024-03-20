8a172e46e4ec54197c2eda93b1d130f794b53aa7
PCD <pascalpcd@bestpicktechnology.com>
2024-03-20 10:26:13 +0700

    commented import main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css';
 import Box from '@mui/material/Box'; 
-import Main from './Main';
+// import Main from './Main';
 import { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Home from './components/Home';
+import Information from './components/Information';
+import PrivateRoute from './PrivateRoutes';
+import Projects from './PrivateRoutes/Projects';
 // import AuthProvider from './AuthProvider';
  
 export default function FixedBottomNavigation() {
+
+  const Main = ({ isAuthenticated }: { isAuthenticated: boolean }): JSX.Element => { 
+    let routes = useRoutes([
+      { path: '/', element: <Home /> },
+      { path: '/info', element: <Information /> },
+      { path: '/projects', element: <PrivateRoute isAuthenticated={isAuthenticated} children={<Projects />} /> },
+    ]);
+  
+    return <div>{routes}</div>;
+  };
 
   const location = useLocation();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
